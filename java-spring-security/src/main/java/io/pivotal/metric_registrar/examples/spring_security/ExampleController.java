c7fd457454e0bbf97c5bfe2f5ef242cb90c771f5
Matthew Heidemann <mheidemann@pivotal.io>
2019-07-12 14:30:53 -0600

    Updated custom metric to gauge and updated the UI

--- java-spring-security/src/main/java/io/pivotal/metric_registrar/examples/spring_security/ExampleController.java
+++ java-spring-security/src/main/java/io/pivotal/metric_registrar/examples/spring_security/ExampleController.java
@@ -1,34 +1,48 @@
 package io.pivotal.metric_registrar.examples.spring_security;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Counter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class ExampleController {
 
     private MeterRegistry registry;
+    private AtomicLong customGauge; 
+    private Counter simpleCounter;
 
     public ExampleController(MeterRegistry registry) {
         this.registry = registry;
+        this.customGauge = registry.gauge("custom", new AtomicLong(0));
+        this.simpleCounter = registry.counter("simple");
     }
 
     @GetMapping("/high_latency")
     public ResponseEntity<String> highLatency() throws InterruptedException {
         Thread.sleep(2000);
-        return new ResponseEntity<>("OK", null, HttpStatus.OK);
+        return new ResponseEntity<>("{}", null, HttpStatus.OK);
     }
 
     @GetMapping("/custom_metric")
-    public ResponseEntity<String> customMetric() {
-        this.registry.counter("custom_metric").increment();
-        return new ResponseEntity<>("OK", null, HttpStatus.OK);
+    public ResponseEntity<String> customMetric(@RequestParam(value="inc", defaultValue="") String increment) {
+        if (!"".equals(increment)) {
+            this.customGauge.incrementAndGet();
+        } else {
+            this.customGauge.decrementAndGet();
+        }
+        
+        return new ResponseEntity<>("{}", null, HttpStatus.OK);
     }
 
     @GetMapping("/simple")
     public ResponseEntity<String> simple() {
-        return new ResponseEntity<>("OK", null, HttpStatus.OK);
+        this.simpleCounter.increment();
+        return new ResponseEntity<>("{}", null, HttpStatus.OK);
     }
 }
